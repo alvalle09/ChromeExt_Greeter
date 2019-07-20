eaead7b514411991f42665300177bcf24b06ebc5
alvalle09 <phredvalle@gmail.com>
2019-07-20 16:47:47 -0500

    final touches on styles, save username to localstorage

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 
-document.getElementById("settings-button").addEventListener('click', openSettings);
 
+var userName; 
+var userName = localStorage.getItem('receivedName');
+
+if (userName == null) {
+    userName = "friend";
+ }
+
+
+document.getElementById("settings-button").addEventListener('click', openSettings);
 
 function openSettings() {
     document.getElementById("settings").classList.toggle("settings-open");
 }
+
+function saveName() {
+    
+    localStorage.setItem('receivedName', userName);
+}
+
+function changeName() {
+    userName = document.getElementById("name-input").value;
+    saveName();
+    getGreeting();
+}
+
+document.getElementById("name-form").addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    changeName();
+})
+
+function getGreeting() {
+    document.getElementById("greeting").innerHTML = `Hello, ${userName}. Enjoy your day!`;
+}
+
+getGreeting();
