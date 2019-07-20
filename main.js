9ecc041012196cbb2e535b9cda680a596f69f38a
alvalle09 <phredvalle@gmail.com>
2019-07-20 15:32:29 -0500

    add styling, toggle settings using js

---
+++ main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("settings-button").addEventListener('click', openSettings);
+
+
+function openSettings() {
+    document.getElementById("settings").classList.toggle("settings-open");
+}
